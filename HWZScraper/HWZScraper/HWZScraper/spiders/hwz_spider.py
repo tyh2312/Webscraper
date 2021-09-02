6ae0b3973a0b4fdf9506414beff84dfe260667fe
tyh2312 <31516640+tyh2312@users.noreply.github.com>
2021-09-02 21:40:56 +0800

    update spider

--- HWZScraper/HWZScraper/HWZScraper/spiders/hwz_spider.py
+++ HWZScraper/HWZScraper/HWZScraper/spiders/hwz_spider.py
@@ -11,26 +11,24 @@ class JapanSpider(scrapy.Spider):
     start_urls = ["https://forums.hardwarezone.com.sg/forums/pc-gaming.382/"]
 
     def parse(self, response):
-        for thread in response.xpath('//div[has-class("js-threadList")]/div[has-class("structItem--thread")]'):
+        for thread in response.css('.js-threadList > .structItem--thread'):
 
             thread_link =  thread.css('.structItem-title a::attr(href)').get()
             
             yield response.follow("https://forums.hardwarezone.com.sg" + thread_link, self.parse_thread)
         
-        next_page = response.css('a.pageNav-jump--next::attr(href)').get
-
+        next_page = response.css('a.pageNav-jump--next::attr(href)').get()
         if next_page is not None:
-            yield response.follow(next_page, self.parse)
+            yield response.follow("https://forums.hardwarezone.com.sg" + next_page, self.parse)
 
     def parse_thread(self, response):
-        topic = response.css('h1.p-title-value::text').get()
+        title = response.css('h1.p-title-value::text').get()
         for post in response.css('.message--post'):
             
             yield {
-                'topic': topic,
-                'author': post.css('.username::text').get(),
+                'title': title,
                 'content': post.css('.bbWrapper::text').get(),
-                
+                'author': post.css('.username::text').get(),
             }
         
         next_page = response.css('a.pageNav-jump--next::attr(href)').get()
