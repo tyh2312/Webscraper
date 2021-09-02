c4c840006970b00a59cc21e6cd12f2279f783259
tyh2312 <31516640+tyh2312@users.noreply.github.com>
2021-09-03 06:39:29 +0800

    change settings

--- HWZScraper/HWZScraper/HWZScraper/spiders/hwz_spider.py
+++ HWZScraper/HWZScraper/HWZScraper/spiders/hwz_spider.py
@@ -11,26 +11,27 @@ class HWZoneSpider(scrapy.Spider):
     start_urls = ["https://forums.hardwarezone.com.sg/forums/pc-gaming.382/"]
 
     def parse(self, response):
-        for thread in response.css('.js-threadList > .structItem--thread'):
+        for thread in response.xpath("//div[@class='structItem-title']/a"):
+            thread_link = thread.xpath("@href").extract()
 
-            thread_link =  thread.css('.structItem-title a::attr(href)').get()
-            
-            yield response.follow("https://forums.hardwarezone.com.sg" + thread_link, self.parse_thread)
-        
-        next_page = response.css('a.pageNav-jump--next::attr(href)').get()
+            yield response.follow("https://forums.hardwarezone.com.sg" + thread_link[0], self.parse_thread)
+
+        next_page = response.xpath("//a[@class='pageNav-jump pageNav-jump--next']/@href").extract()
         if next_page is not None:
-            yield response.follow("https://forums.hardwarezone.com.sg" + next_page, self.parse)
+            yield response.follow("https://forums.hardwarezone.com.sg" + next_page[0], self.parse)
 
     def parse_thread(self, response):
-        title = response.css('h1.p-title-value::text').get()
-        for post in response.css('.message--post'):
-            
+        title = response.xpath("//h1[@class='p-title-value']/text()").extract()
+        author = response.xpath("//a[@itemprop='name']/text()").extract()
+        content = response.xpath("//div[@class='bbWrapper']/text()").extract()
+
+        for post in range(len(response.xpath("//article[@class='message message--post js-post js-inlineModContainer  ']"))):
             yield {
-                'title': title,
-                'content': post.css('.bbWrapper::text').get(),
-                'author': post.css('.username::text').get(),
+                'title': title[0],
+                'author': author[post],
+                'content':content[post],
             }
-        
-        next_page = response.css('a.pageNav-jump--next::attr(href)').get()
+
+        next_page = response.xpath("//a[@class='pageNav-jump pageNav-jump--next']/@href").extract()
         if next_page is not None:
-            yield response.follow("https://forums.hardwarezone.com.sg" + next_page, self.parse_thread)
+            yield response.follow("https://forums.hardwarezone.com.sg" + next_page[0], self.parse_thread)
