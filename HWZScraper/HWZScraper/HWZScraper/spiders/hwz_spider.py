5b277f122f92928cddbf38a49404d7a08a18afa7
tyh2312 <31516640+tyh2312@users.noreply.github.com>
2021-09-03 11:45:46 +0800

    update spider file for selector

--- HWZScraper/HWZScraper/HWZScraper/spiders/hwz_spider.py
+++ HWZScraper/HWZScraper/HWZScraper/spiders/hwz_spider.py
@@ -12,25 +12,25 @@ class HWZoneSpider(scrapy.Spider):
     # specify url of page to crawl posts from
     start_urls = ["https://forums.hardwarezone.com.sg/forums/pc-gaming.382/"]
 
-    # custom settings
+    # custom settings to set logging level
     custom_settings = {
         'LOG_LEVEL': logging.WARNING
     }
 
     def parse(self, response):
         for thread in response.xpath("//div[@class='structItem-title']/a"):
-            thread_link = thread.xpath("@href").extract()
+            thread_link = thread.xpath("@href").get()
 
             yield response.follow("https://forums.hardwarezone.com.sg" + thread_link[0], self.parse_thread)
 
-        next_page = response.xpath("//a[@class='pageNav-jump pageNav-jump--next']/@href").extract()
+        next_page = response.xpath("//a[@class='pageNav-jump pageNav-jump--next']/@href").get()
         if next_page is not None:
             yield response.follow("https://forums.hardwarezone.com.sg" + next_page[0], self.parse)
 
     def parse_thread(self, response):
-        title = response.xpath("//h1[@class='p-title-value']/text()").extract()
-        author = response.xpath("//a[@itemprop='name']/text()").extract()
-        content = response.xpath("//div[@class='bbWrapper']/text()").extract()
+        title = response.xpath("//h1[@class='p-title-value']/text()").get()
+        author = response.xpath("//a[@itemprop='name']/text()").get()
+        content = response.xpath("//div[@class='bbWrapper']/text()").get()
 
         for post in range(len(response.xpath("//article[@class='message message--post js-post js-inlineModContainer  ']"))):
             yield {
@@ -39,6 +39,6 @@ class HWZoneSpider(scrapy.Spider):
                 'content':content[post],
             }
 
-        next_page = response.xpath("//a[@class='pageNav-jump pageNav-jump--next']/@href").extract()
+        next_page = response.xpath("//a[@class='pageNav-jump pageNav-jump--next']/@href").get()
         if next_page is not None:
             yield response.follow("https://forums.hardwarezone.com.sg" + next_page[0], self.parse_thread)
